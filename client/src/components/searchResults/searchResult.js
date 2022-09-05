8d7dcfe253e37194fa26aa58b9844b14b27ea0f1
Tais Calisto <taiscalisto97@gmail.com>
2022-09-05 10:10:33 -0400

    Styled search results

--- client/src/components/searchResults/searchResult.js
+++ client/src/components/searchResults/searchResult.js
@@ -1,12 +1,15 @@
 import React from 'react'
+import { IconCirclePlus } from '@tabler/icons'
 
 const SearchResult = (book) => {
-  console.log(book)
   return (
     <>
       <li>
         <h4>{book.title}</h4>
         <img src={book.cover} alt={book.title} />
+        <button>
+          <IconCirclePlus />
+        </button>
       </li>
     </>
   )
--- client/src/styles/SearchResults.styles.js
+++ client/src/styles/SearchResults.styles.js
@@ -19,6 +19,26 @@ export const StyleSearchResults = styled.ul`
 
   li {
     list-style: none;
+    border: 2px solid ${({ theme }) => theme.colors.lightPink};
+    border-radius: 10px;
+    transition: all 0.2s ease-in-out;
+    &:hover {
+      scale: 1.05;
+      background-color: ${({ theme }) => theme.colors.lightPink};
+    }
+    button {
+      display: flex;
+      width: 100%;
+      justify-content: end;
+      background-color: transparent;
+      border: none;
+      color: ${({ theme }) => theme.colors.darkPink};
+      transition: all 0.2s ease-in-out;
+      cursor: pointer;
+      &:hover {
+        color: ${({ theme }) => theme.colors.black};
+      }
+    }
   }
 
   @media screen and (min-width: 1024px) {
