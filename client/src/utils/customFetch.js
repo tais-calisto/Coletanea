1a199d00386fcdbdeb6982676b7d32cf1298251a
Tais Calisto <taiscalisto97@gmail.com>
2022-09-16 16:23:32 -0400

    Updated API URL

--- client/src/utils/customFetch.js
+++ client/src/utils/customFetch.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
 
-const customFetch = axios.create({ baseURL: 'http://localhost:5000/api/v1' })
+const customFetch = axios.create({ baseURL: 'http://coletanea/api/v1' })
 
 export default customFetch
