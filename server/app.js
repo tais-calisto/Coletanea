af6416f6bfe600e9ecd9af09dc6a1e56d1d08643
Tais Calisto <taiscalisto97@gmail.com>
2022-09-16 16:13:06 -0400

    Configured cors

--- server/app.js
+++ server/app.js
@@ -9,12 +9,13 @@ import authRouter from './routes/authentication.js'
 import goalRouter from './routes/goals.js'
 import authentication from './middleware/authentication.js'
 import cors from 'cors'
+import corsOptions from './config/corsOptions.js'
 
 dotenv.config()
 const app = express()
 
 app.use(express.json())
-app.use(cors())
+app.use(cors(corsOptions))
 
 app.use('/api/v1/auth', authRouter)
 app.use('/api/v1/books', authentication, booksRouter)
---
+++ server/config/allowedOrigins.js
@@ -0,0 +1,6 @@
+const allowedOrigins = [
+  'http://localhost:3000',
+  'https://coletanea.onrender.com',
+]
+
+export default allowedOrigins
