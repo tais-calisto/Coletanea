4210265ff44c28b36043d8c322c05df04e513a1c
Tais Calisto <taiscalisto97@gmail.com>
2022-09-16 16:50:16 -0400

    Updated baseURL

--- client/src/utils/customFetch.js
+++ client/src/utils/customFetch.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const customFetch = axios.create({
-  baseURL: 'https://coletanea-api.onrender.com',
+  baseURL: 'https://coletanea-api.onrender.com/api/v1',
 })
 
 export default customFetch
